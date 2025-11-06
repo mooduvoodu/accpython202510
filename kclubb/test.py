cfa376181c66d249208f77c6123998d56e622aec
Kyle Clubb <kclubb@austincc.edu>
2025-11-06 01:04:23 +0000

    updates

--- kclubb/test.py
+++ kclubb/test.py
@@ -1,1 +1,25 @@
-print('my name');
+import math
+import numpy
+
+var1 = 4
+var2 = 'kyle'
+var3 = [1,2,4,5]
+var4 = ('a','bd')
+
+print(var1)
+var3.append(99)
+
+var3[2:4]
+
+def funky(param1,param2 = '4'):
+    pass
+
+numpy.ndarray()
+
+
+
+
+
+
+
+
